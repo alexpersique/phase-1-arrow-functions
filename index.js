121555eae2552d6ea2a5691abce4420a8de325d6
Alexander <alexdgenkin@gmail.com>
2024-02-08 05:54:56 +1100

    Fills index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function (dividend,divisor) {
+    return dividend / divisor;
+
+}
+
+console.log(divide(2000,100));
+
+const square = (factor) => factor*factor;
+
+const add = (number1, number2) => number1 + number2;
+
+console.log (square(3));
+console.log (add (4,7));
+
